85dd7cc2e0e79c57d7728fbb9b62183e4f479fd3
abjaigirdar <abjaigirdar@gmail.com>
2023-02-15 17:14:46 -0500

    style updated

--- src/components/Tag_V1.jsx
+++ src/components/Tag_V1.jsx
@@ -10,7 +10,7 @@ const Tag = () => {
     return (
         <div className="container">
             <h1 className = 'new' >Random [ {tag} ] Gif</h1>
-            <img width = '500' height='350' src={gif} alt='random_GIF' />
+            <img width = '500' height='350' src={gif} alt='random_GIF' className='gif--div'/>
             <div className='details'>
             <h2 className='search--label'>&#129147; Enter a Word or phrase to search &#129147;</h2>
             <input value={tag} onChange = {(e)=> setTag(e.target.value)} className="input"/>
