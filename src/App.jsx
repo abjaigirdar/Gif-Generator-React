eb2e4c5c8ef52e75b4dfb2918657a477218ca70b
abjaigirdar <abjaigirdar@gmail.com>
2023-02-15 22:38:11 -0500

    adjusted media queries

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,8 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-      <div className="main-container">
-      <Tag />
+      <Tag className="hero--main"/>
       <Footer/>
-      </div>
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
         <a href='https://www.linkedin.com/in/jaigirdar/' target="_blank"><i class="fa-brands fa-linkedin"></i></a>
         <a href='https://github.com/abjaigirdar' target="_blank"><i class="fa-brands fa-github"></i></a>
         </div>
-        <h3><i class="fa-sharp fa-solid fa-chevron-left"></i> Abrar Jaigirdar <i class="fa-sharp fa-solid fa-chevron-right"></i></h3>
+        <h3>Abrar Jaigirdar © 2023, J brigade</h3>
     </div>
   )
 }
